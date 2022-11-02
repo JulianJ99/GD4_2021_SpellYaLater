c6c6db1fc434eb2e5afec301f90fa4269528f82c
JulianJ99 <465268@student.fontys.nl>
2022-11-02 11:28:50 +0100

    Checkpoint fix + GrappleCanvas on Staff

--- Spell Ya Later!/Assets/Scripts/GrappleStaff.cs
+++ Spell Ya Later!/Assets/Scripts/GrappleStaff.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class GrappleStaff : MonoBehaviour
 {
     public GameObject GunPlace;
+    public GameObject GrappleCanvas;
     public AudioClip ItemClip;
     public AudioSource PlayerSource;
 
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == "Player"){
             GunPlace.SetActive(true);
+            GrappleCanvas.SetActive(true);
             Destroy(gameObject);
             PlayerSource.pitch = 1f;
             PlayerSource.PlayOneShot(ItemClip);
